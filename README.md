48f8fd694ea5fb4d723fe56c4fa571de0626c055
Gudrunbjork01 <annagudrun01@gmail.com>
2021-10-21 04:52:21 +0000

    initial commit

--- README.md
+++ README.md
@@ -68,6 +68,24 @@ git
 ### Frameworks, Libraries & Programs Used
 
 
+- quiz.js 
+<img width="1165" alt="Skärmavbild 2021-10-21 kl 06 03 41" src="https://user-images.githubusercontent.com/82470469/138209698-82f5dccb-acc3-4d5b-8203-cebfd246c26b.png">
+
+- end.js 
+<img width="938" alt="Skärmavbild 2021-10-21 kl 06 11 23" src="https://user-images.githubusercontent.com/82470469/138210246-d04d831d-f95e-49b4-a778-8f2b599a6fd3.png">
+
+- index.html 
+<img width="1366" alt="Skärmavbild 2021-10-21 kl 06 15 56" src="https://user-images.githubusercontent.com/82470469/138210600-d2d38f39-0645-4dad-b16e-e32c77818c5d.png">
+
+- quiz.html
+<img width="1381" alt="Skärmavbild 2021-10-21 kl 06 17 12" src="https://user-images.githubusercontent.com/82470469/138210737-8a933664-f672-4064-9522-6d4bda1dff8a.png">
+
+- end.html 
+<img width="1406" alt="Skärmavbild 2021-10-21 kl 06 18 30" src="https://user-images.githubusercontent.com/82470469/138210832-56280884-64eb-4035-bbb1-de9b8453c2dc.png">
 
 
+- style.css 
+<img width="1406" alt="Skärmavbild 2021-10-21 kl  06 18 30" src="https://user-images.githubusercontent.com/82470469/138213296-9d89bf81-78c2-4b1a-8e58-8fd80d6c1f8b.png">
 
+- quiz.css 
+<img width="1067" alt="Skärmavbild 2021-10-21 kl  06 50 22" src="https://user-images.githubusercontent.com/82470469/138213485-e41b4c7f-5caa-44e0-be40-a359ef10e5a6.png">
--- quiz.js
+++ quiz.js
@@ -5,125 +5,121 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 
 /* declareing my variables*/
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0 
-let questionCounter = 0 
-let availableQuestions = []
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0 ;
+let questionCounter = 0 ;
+let availableQuestions = [];
 
 /* my questions and answers*/ 
 let questions = [
-    {
-        question: 'Who is the oldest of the kardashian/ Jenner siblings?',
-        choice1: 'Kendall Jenner',
-        choice2: 'Rob Kardashian',
-        choice3: 'Kortney Kardashian',
-        choice4: 'Kim Kardashian',
-        answer: 3, 
-    },   
-    {
-        question: 'What is the name of the kardashian sisters father?',
-        choice1: 'Rob Kardashian',
-        choice2: 'Bruce Jenner',
-        choice3: 'Caitlin Jenner',
-        choice4: 'Robert Kardashian',
-        answer: 4, 
-    },
-    {
-        question: 'Where did Kim Kardashian loose her famous earring?',
-        choice1: 'Bora Bora',
-        choice2: 'Maldives',
-        choice3: 'Hawaii',
-        choice4: 'Bali',
-        answer: 1, 
-    },
-    {
-        question: 'How many Kardashian/ jenner grandkids are there?',
-        choice1: '7',
-        choice2: '4',
-        choice3: '10',
-        choice4: '12',
-        answer: 3, 
-    },
-]
- 
+
+{
+question: 'Who is the oldest of the kardashian/ Jenner siblings?',
+choice1: 'Kendall Jenner',
+choice2: 'Rob Kardashian',
+choice3: 'Kortney Kardashian',
+choice4: 'Kim Kardashian',
+answer: 3, 
+}, 
+
+{
+question: 'What is the name of the kardashian sisters father?',
+choice1: 'Rob Kardashian',
+choice2: 'Bruce Jenner',
+choice3: 'Caitlin Jenner',
+choice4: 'Robert Kardashian',
+answer: 4, 
+},
+
+{
+question: 'Where did Kim Kardashian loose her famous earring?',
+choice1: 'Bora Bora',
+choice2: 'Maldives',
+choice3: 'Hawaii',
+choice4: 'Bali',
+answer: 1, 
+},
+
+{
+question: 'How many Kardashian/ jenner grandkids are there?',
+choice1: '7',
+choice2: '4',
+choice3: '10',
+choice4: '12',
+answer: 3, 
+},
+
+];
 /* my scorepoints and questions */
-const SCORE_POINTS  = 100
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
 
-
-startGame = () => {
-    questionCounter = 0 
-    score = 0 
-    availableQuestions = [...questions]
-    getNewQuestion ()
-}
+var startGame = () => {
+questionCounter = 0 ;
+score = 0 ;
+availableQuestions = [...questions];
+getNewQuestion ();
+};
 
 /* keeping track of my scores*/
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
-
-        return window.location.assign('/end.html')
-    }
+var getNewQuestion = () => {
+if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+localStorage.setItem('mostRecentScore', score);
+return window.location.assign('/end.html');
+}
 
 /* keeping track of my questions*/
-    questionCounter++ 
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+questionCounter++ ;
+progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
 
 /* keeping track of question you are at */
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-    
- /* choice tracker*/
-        choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
+const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+currentQuestion = availableQuestions[questionsIndex];
+question.innerText = currentQuestion.question;
+
+/* choice tracker*/
+choices.forEach(choice => {
+const number = choice.dataset.number;
+choice.innerText = currentQuestion['choice' + number];
+});
+
 /* keeping track of my questions*/
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
-    
-
-    function newFunction() {
-        progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-    }
-}
+availableQuestions.splice(questionsIndex, 1);
+acceptingAnswers = true;
+};
 
 choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+choice.addEventListener('click', e => {
+if(!acceptingAnswers) return;
 
-        /* choice A, B, C ,D*/
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+/* choice A, B, C ,D*/
+acceptingAnswers = false;
+const selectedChoice = e.target;
+const selectedAnswer = selectedChoice.dataset.number;
 
-        /* targeting red an green css */
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+/* targeting red an green css */
+let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-        /* increases score by 100% (100points)*/
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply)
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-
-        }, 1000)
-    })
-})
-
-incrementScore = num => {
-    score +=num
-    scoreText.innerText = score
+/* increases score by 100% (100points)*/
+if(classToApply === 'correct') {
+incrementScore(SCORE_POINTS);
 }
 
-startGame ()
+selectedChoice.parentElement.classList.add(classToApply);
+setTimeout(() => {
+selectedChoice.parentElement.classList.remove(classToApply);
+getNewQuestion();
 
+}, 1000);
+
+});
+
+});
+
+const incrementScore = num => {
+score +=num;
+scoreText.innerText = score;
+};
+startGame ();
 
